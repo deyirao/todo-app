1c7fdd095a724b53479c96fa51f6134f174e4f7e
Deyi <deyi.rao@gmx.de>
2023-08-21 18:23:32 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import Task from './Task';
+import TaskForm from './TaskForm';
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    if (tasks.length === 0) return;
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }, [tasks]);
+
+  useEffect(() => {
+    const tasks = JSON.parse(localStorage.getItem('tasks'));
+    setTasks(tasks);
+  }, []);
+
+  function addTask(name) {
+    setTasks(prev => {
+      return [...prev, {name: name, done: false}];
+    });
+  }
+
+  function updateTaskDone(taskIndex, newDone) {
+    setTasks(prev => {
+      const newTasks = [...prev];
+      newTasks[taskIndex].done = newDone;
+      return newTasks;
+    });
+  }
+
+  const numberComplete = tasks.filter(t => t.done).length;
+  const numberTotal = tasks.length;
+
+  function getMessage() {
+    if (numberComplete === 0) {
+      return 'Gib dein Bestes! 😉';
+    }
+    if (numberComplete === numberTotal) {
+      return 'Feierabend!! 🍻';
+    }
+    return 'Weiter so! 💪';
+  }
+
+  function removeTask(indexToRemove) {
+    setTasks(prev => {
+      return prev.filter((taskObject, index) => index !== indexToRemove);
+    });
+  }
+
+  function renameTask(index, newName) {
+    setTasks(prev => {
+      const newTasks = [...prev];
+      newTasks[index].name = newName;
+      return newTasks;
+    });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1>{numberComplete}/{numberTotal} Fertig</h1>
+      <h2>{getMessage()}</h2>
+      <TaskForm onAdd={addTask}/>
+      {tasks.map((task, index) => (
+        <Task {...task} 
+        onRename={newName => renameTask(index, newName)}
+        onTrash={() => removeTask(index)}
+        onToggle={done => updateTaskDone(index, done)}/>
+      ))}
+    </main>
   );
 }
 
